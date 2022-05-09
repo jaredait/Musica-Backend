49c42fc886160e0c5d4b164505c3f4d6b31981bc
jaredait <jaredampudia@gmail.com>
2022-05-09 13:17:02 -0500

    Agregar CORS

--- Musica/Musica/Controllers/CancionController.cs
+++ Musica/Musica/Controllers/CancionController.cs
@@ -6,9 +6,11 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Musica.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class CancionController : ApiController
     {
         ICancion _cancionModelo;
--- Musica/Musica/Controllers/GeneroController.cs
+++ Musica/Musica/Controllers/GeneroController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Musica.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class GeneroController : ApiController
     {
         IGenero _generoModelo;
